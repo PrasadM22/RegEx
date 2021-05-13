1efd907f730d408f18a47032931ad35f22cc1663
RagahavM22 <pmurhekar8@gmail.com>
2021-05-14 00:22:32 +0530

    Add File

---
+++ UserRegistration/src/Problem/Registration.java
@@ -0,0 +1,23 @@
+package Problem;
+import java.util.*;
+import java.util.regex.*;
+
+public class Registration {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Enter The first Name");
+	String Fname = sc.nextLine();
+	if(Pattern.matches("[A-Z]{1}[a-z]{2,}",Fname))
+	{
+		System.out.println("Name is Valid");
+	}
+	else
+	{
+		System.out.println("Invalid Name. Enter proper Name");
+	}
+}
+}
+
+
+

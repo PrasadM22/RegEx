d32b825c1425263187e178de70eecf4db7ee1b81
RagahavM22 <pmurhekar8@gmail.com>
2021-05-14 00:31:44 +0530

    Changes made

--- UserRegistration/src/Problem/Registration.java
+++ UserRegistration/src/Problem/Registration.java
@@ -3,9 +3,8 @@ import java.util.*;
 import java.util.regex.*;
 
 public class Registration {
+	static Scanner sc = new Scanner(System.in);
 public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	
 	System.out.println("Enter The first Name");
 	String Fname = sc.nextLine();
 	if(Pattern.matches("[A-Z]{1}[a-z]{2,}",Fname))
@@ -15,7 +14,20 @@ public static void main(String[] args) {
 	else
 	{
 		System.out.println("Invalid Name. Enter proper Name");
+		
 	}
+	//Validate last name
+	System.out.println("Enter The last Name");
+	String Lname = sc.nextLine();
+	if(Pattern.matches("[A-Z]{1}[a-z]{2,}",Lname))
+	{
+		System.out.println("Name is Valid");
+	}
+	else
+	{
+		System.out.println("Invalid Name.Enter proper Name");
+	}
+	
 }
 }
 
